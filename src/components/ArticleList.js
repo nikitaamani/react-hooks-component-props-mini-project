e57ccef1c5ca336ab03a96edf4ee01f8f67db02f
nikita <nikita.bosire@student.moringaschool.com>
2024-08-20 19:14:23 +0300

    complete lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,22 @@
+
+// src/components/ArticleList.js
+import React from 'react';
+import Article from './Article';
+
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post, index) => (
+        <Article 
+          key={index}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+          minutes={post.minutes}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default ArticleList;
